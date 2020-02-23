0c7d0cd83313b093f9fa49b82c466a8a0f83866d
Vinícius Gomes <vinicius.gouveia.gomes@gmail.com>
2020-02-23 19:56:01 -0300

    Refatorações e mudanças no tema da aplicação e no logo

--- src/app/layout/toolbar/toolbar.component.ts
+++ src/app/layout/toolbar/toolbar.component.ts
@@ -21,8 +21,8 @@ export class ToolbarComponent implements OnInit
   
   backgrounds = 
   {
-    login: 'lightpink',
-    novaconta: '#7941a1'// '#4b0082bd'
+    login: '#ffb6c1ad', //#f873ad (v2)   #ffb6c1ad   #fcd4da   lightpink
+    novaconta: '#8341a1d4'//  #8341a1   #8341a1d4   #7941a1a1   #7941a1   '#4b0082bd'
   }
 
   constructor(private breakpointObserver: BreakpointObserver,
